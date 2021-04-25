61d90eb6842ece6e968d4b4f7f4e68a3da4788ed
marinagilimovich <66743245+marinagilimovich@users.noreply.github.com>
2021-04-25 22:10:17 +0200

    Downloading the file

---
+++ src/test/java/tests/DownloadedFileTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import utils.Files;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
+public class DownloadedFileTest {
+    @Test
+    void selenideDownloadReadmeTest() throws IOException {
+        Configuration.downloadsFolder = "downloads";
+
+        open("https://github.com/selenide/selenide/blob/master/README.md");
+        File downloadedFile = $("#raw-url").download();
+        String fileContent = Files.readTextFromFile(downloadedFile);
+
+        assertThat(fileContent, containsString("Selenide = 1UI Testing Framework powered by Selenium WebDriver"));
+    }
+}
